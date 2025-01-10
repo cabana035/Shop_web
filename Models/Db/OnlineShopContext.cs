e472f534a1dbb527f4235e8e60bfe87496777a1f
cabana <amir.kouchaki@gmail.com>
2025-01-10 13:59:40 +0330

    productstart

--- Models/Db/OnlineShopContext.cs
+++ Models/Db/OnlineShopContext.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Shop_web.Models.Db;
 
-public partial class OnlineShopContext : DbContext
+public partial class OnlineshopContext : DbContext
 {
-    public OnlineShopContext()
+    public OnlineshopContext()
     {
     }
 
-    public OnlineShopContext(DbContextOptions<OnlineShopContext> options)
+    public OnlineshopContext(DbContextOptions<OnlineshopContext> options)
         : base(options)
     {
     }
@@ -19,9 +19,13 @@ public partial class OnlineShopContext : DbContext
 
     public virtual DbSet<Menu> Menus { get; set; }
 
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<ProductGalery> ProductGaleries { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=.;Database=OnlineShop;Trusted_Connection=True;TrustServercertificate=true");
+        => optionsBuilder.UseSqlServer("server=.;database=Onlineshop;trusted_connection=true;trustservercertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -43,6 +47,30 @@ public partial class OnlineShopContext : DbContext
             entity.Property(e => e.Link).HasMaxLength(300);
             entity.Property(e => e.MenuTitle).HasMaxLength(300);
             entity.Property(e => e.Type).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Discount).HasColumnType("money");
+            entity.Property(e => e.FullDesc).HasMaxLength(4000);
+            entity.Property(e => e.ImageName).HasMaxLength(50);
+            entity.Property(e => e.Price).HasColumnType("money");
+            entity.Property(e => e.Tags).HasMaxLength(1000);
+            entity.Property(e => e.Title).HasMaxLength(200);
+            entity.Property(e => e.VideoUrl).HasMaxLength(300);
+        });
+
+        modelBuilder.Entity<ProductGalery>(entity =>
+        {
+            entity.ToTable("ProductGalery");
+
+            entity.Property(e => e.ImageName).HasMaxLength(50);
+
+            entity.HasOne(d => d.Product).WithMany(p => p.ProductGaleries)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ProductGalery_Products");
         });
 
         OnModelCreatingPartial(modelBuilder);
